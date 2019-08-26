f306cace503aa405b888b4c9ad50d07844b07759
gforti <gforti@its.jnj.com>
2019-08-26 17:16:01 -0400

    switched to dotenv-extended

--- templates/scaffold/environment.build.js
+++ templates/scaffold/environment.build.js
@@ -1,5 +1,8 @@
 const { resolve } = require('path')
-require('dotenv').config({ path: resolve(__dirname, '.env'), })
+require('dotenv-extended').load({
+  errorOnMissing: true,
+  path: resolve(__dirname, '.env')
+})
 const UI_ENV_VARS = Object.entries(process.env)
   .filter(([key]) => key.startsWith('UI_'))
   .reduce((acc, [key, val]) =>
